a8328aa3f907316cb0fb951993fb24fe80c54baa
Anton Tihanin <tihanin_ne@mail.ru>
2020-01-15 13:17:41 +0300

    added files

---
+++ 2/default.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Test</title>
+</head>
+<body>
+	<div class="download-section">
+		<h1>This link should be open only on the mobile platform</h1>
+	</div>
+</body>
+</html>
---
+++ 2/index.php
@@ -0,0 +1,5 @@
+<?php
+require_once __DIR__ . '/vendor/mobiledetect/mobiledetectlib/Mobile_Detect.php';
+$detect = new Mobile_Detect;
+
+require_once __DIR__ . '/default.php';
