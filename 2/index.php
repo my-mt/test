aa265b312065d4f264fa5d7e8319524010ccd439
Anton Tihanin <tihanin_ne@mail.ru>
2020-03-25 12:36:22 +0300

    fixed 53-2

--- 2/index.php
+++ 2/index.php
@@ -3,5 +3,6 @@ require_once __DIR__ . '/vendor/mobiledetect/mobiledetectlib/Mobile_Detect.php';
 $detect = new Mobile_Detect;
 
 // поправил проблему 53
+// поправил проблему 53 - 2
 
 require_once __DIR__ . '/default.php';
